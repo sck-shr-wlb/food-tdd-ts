801835fb67427c6a10cc887d2070196c5eb43dea
pachara.mee <pachara.mee@cpf.co.th>
2023-02-08 08:52:43 +0700

    [feature] add should return V when input 5

--- src/romanNumerals.ts
+++ src/romanNumerals.ts
@@ -1,17 +1,18 @@
 export const convert = (input: number): string => {
-    let i = 0;
+
     let textRoman = "";
 
-    if(input == 4){
+    if (input == 4) {
         return "IV";
     }
 
-    for (i = 0; i < input; i++) {
-        textRoman+="I";
-
+    if (input == 5) {
+        return "V";
     }
 
+    for (let i = 0; i < input; i++) {
+        textRoman += "I";
+    }
 
     return textRoman;
-
 }
